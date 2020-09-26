98cf8f396a7c0a8da5c5b51829e5fc312d900cad
Utkarsh Sharma <skhiearth@gmail.com>
2020-09-26 20:38:58 +0530

    Create Bridging-Header.h

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  Pink+
+//
+//  Created by Utkarsh Sharma on 26/09/20.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <JSQMessagesViewController/JSQMessages.h>
+
+
+#endif /* Bridging_Header_h */
