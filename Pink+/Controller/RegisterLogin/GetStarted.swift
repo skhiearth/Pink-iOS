9de998aa6795525978d8de3cf4fca8cc62919cf7
Utkarsh Sharma <skhiearth@gmail.com>
2020-09-26 17:22:35 +0530

    authentication

---
+++ Pink+/Controller/RegisterLogin/GetStarted.swift
@@ -0,0 +1,29 @@
+//
+//  GetStarted.swift
+//  Pink+
+//
+//  Created by Utkarsh Sharma on 18/09/20.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class GetStarted: UIViewController {
+    
+    //MARK: IBOutlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        
+    }
+    
+    // MARK: IBAction
+    @IBAction func getStartedButtonTapped(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "RegisterOne") as! RegisterOne
+        self.present(nextViewController, animated:true, completion:nil)
+    }
+    
+}
