6281edd0d9a2952e021898476832020502d180c8
Utkarsh Sharma <skhiearth@gmail.com>
2020-09-27 13:40:39 +0530

    watchOS Icons

--- Pink+/Controller/AppDelegate.swift
+++ Pink+/Controller/AppDelegate.swift
@@ -7,17 +7,34 @@
 
 import UIKit
 import Firebase
+import WatchConnectivity
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
 
     var window: UIWindow?
+    var session: WCSession?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        self.configureWatchKitSesstion()
+        
+        if let validSession = self.session, validSession.isReachable {
+            let data: [String: Any] = ["iPhone": "Data from iPhone" as Any]
+            validSession.sendMessage(data, replyHandler: nil, errorHandler: nil)
+        }
+        
         return true
     }
+    
+    func configureWatchKitSesstion() {
+        if WCSession.isSupported() {
+          session = WCSession.default
+          session?.delegate = self
+          session?.activate()
+        }
+      }
 
     // MARK: UISceneSession Lifecycle
 
@@ -32,6 +49,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+    }
+      
+    func sessionDidDeactivate(_ session: WCSession) {
+    }
 
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        print("received message: \(message)")
+    }
 
 }
