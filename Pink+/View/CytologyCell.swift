10dff8093adddfdf00e157a4d2f15ca7a9a03897
Utkarsh Sharma <skhiearth@gmail.com>
2020-09-28 19:07:24 +0530

    Create CytologyCell.swift

---
+++ Pink+/View/CytologyCell.swift
@@ -0,0 +1,22 @@
+//
+//  CytologyCell.swift
+//  Pink+
+//
+//  Created by Utkarsh Sharma on 28/09/20.
+//
+
+import UIKit
+
+class CytologyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
