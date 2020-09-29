70fc6207f983dafcfff1667a68e419ef37bf0095
Utkarsh Sharma <skhiearth@gmail.com>
2020-09-29 17:44:10 +0530

    Create SymptomCell.swift

---
+++ Pink+/View/SymptomCell.swift
@@ -0,0 +1,24 @@
+//
+//  SymptomCell.swift
+//  Pink+
+//
+//  Created by Utkarsh Sharma on 29/09/20.
+//
+
+import UIKit
+
+class SymptomCell: UITableViewCell {
+
+    @IBOutlet weak var idLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
